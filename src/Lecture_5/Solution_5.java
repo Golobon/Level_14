3cb0169d0b2ea839abaa2d331d12153906a306a7
Golobon <golobon@ya.ru>
2022-08-23 21:25:26 +0300

    Commit

---
+++ src/Lecture_5/Solution_5.java
@@ -0,0 +1,27 @@
+package Lecture_5;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution_5 {
+    public static List getListForGet() {
+        return new ArrayList<>();
+    }
+
+    public static List getListForSet() {
+        return new ArrayList<>();
+    }
+
+    public static List getListForAddOrInsert() {
+        return new LinkedList();
+    }
+
+    public static List getListForRemove() {
+        return new LinkedList();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
