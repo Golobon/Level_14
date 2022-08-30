eda8d0150a6ace285ba4191aa072a96275a58248
Golobon <golobon@ya.ru>
2022-08-30 21:11:24 +0300

    Commit

--- src/Test.java
+++ src/Test.java
@@ -1,20 +1,18 @@
-import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class Test {
-    public static void main(String[] args) throws ParseException {
-        int[] arr = new int[]{1000, 900, 800, 5, 1, 2, 300, -142, 123, 12, 32, -300, -400, -500};
-        for (int i = arr.length-1; i >= 1; i--) {
-            int[] arr2 = arr.clone();
-            for (int j = 0; j < i; j++) {
-                if (arr2[j] > arr2[j + 1]) {
-                    int shift = arr2[j];
-                    arr2[j] = arr2[j + 1];
-                    arr2[j + 1] = shift;
-                }
-            }
-            arr = arr2.clone();
-            System.out.println(Arrays.toString(arr));
-        }
+    public static void main(String[] args){
+        var names = Arrays.asList("John", "John", "Jack");
+        names = new ArrayList<>(names);
+        /*Consumer consumer = (obj) -> System.out.println(obj.hashCode());
+        consumer.andThen((obj) -> System.out.print("ff" + "\n"));
+        names.forEach(consumer.andThen((obj) -> System.out.print("ff")));*/
+        Predicate predicate = (obj) -> obj.equals("John");
+        names.removeIf(predicate);
+        System.out.println(names);
     }
 }
