4a550f51837e8d8adebada2c2ee9e3a80d0384f9
Golobon <golobon@ya.ru>
2022-08-31 00:37:06 +0300

    Commit

--- src/Test.java
+++ src/Test.java
@@ -6,13 +6,8 @@ import java.util.function.Predicate;
 
 public class Test {
     public static void main(String[] args){
-        var names = Arrays.asList("John", "John", "Jack");
-        names = new ArrayList<>(names);
-        /*Consumer consumer = (obj) -> System.out.println(obj.hashCode());
-        consumer.andThen((obj) -> System.out.print("ff" + "\n"));
-        names.forEach(consumer.andThen((obj) -> System.out.print("ff")));*/
-        Predicate predicate = (obj) -> obj.equals("John");
-        names.removeIf(predicate);
+        List <String> names = new ArrayList<>(Arrays.asList("John", "Bob", "Jack"));
+        names.remove(1);
         System.out.println(names);
     }
 }
