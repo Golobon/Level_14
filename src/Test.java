1ebdf19f01e7ca28bb0b7538e4a8b09a81f71a85
Golobon <golobon@ya.ru>
2022-08-30 19:59:15 +0300

    Commit

--- src/Test.java
+++ src/Test.java
@@ -1,25 +1,20 @@
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Month;
-import java.util.*;
+import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) throws ParseException {
-        Integer a = new Integer(3);
-        Integer b = new Integer(3);
-        Integer c = 3;
-        Integer d = 3;
-        Integer e = 333;
-        Integer f = 333;
-        System.out.println(a==b);
-        System.out.println(a.equals(b));
-        System.out.println();
-        System.out.println(c==d);
-        System.out.println(c.equals(d));
-        System.out.println();
-        System.out.println(e==f);
-        System.out.println(e.equals(f));
-
+        int[] arr = new int[]{1000, 900, 800, 5, 1, 2, 300, -142, 123, 12, 32, -300, -400, -500};
+        for (int i = arr.length-1; i >= 1; i--) {
+            int[] arr2 = arr.clone();
+            for (int j = 0; j < i; j++) {
+                if (arr2[j] > arr2[j + 1]) {
+                    int shift = arr2[j];
+                    arr2[j] = arr2[j + 1];
+                    arr2[j + 1] = shift;
+                }
+            }
+            arr = arr2.clone();
+            System.out.println(Arrays.toString(arr));
+        }
     }
 }
