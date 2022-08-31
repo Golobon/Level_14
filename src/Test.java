6b6ccc256d93437846b32884dcd29e3d58a38f37
Golobon <golobon@ya.ru>
2022-09-01 00:48:45 +0300

    Commit

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Arrays;
 import java.util.function.Consumer;
@@ -6,8 +7,64 @@ import java.util.function.Predicate;
 
 public class Test {
     public static void main(String[] args){
-        List <String> names = new ArrayList<>(Arrays.asList("John", "Bob", "Jack"));
-        names.remove(1);
-        System.out.println(names);
+        System.out.println(DayOfWeek.values()[0]);
+    }
+
+    public enum DayOfWeek {
+
+        SUNDAY ("Воскресенье"),
+        MONDAY ("Понедельник"),
+        TUESDAY ("Вторник"),
+        WEDNESDAY ("Среда"),
+        THURSDAY ("Четверг"),
+        FRIDAY ("Пятница"),
+        SATURDAY ("Суббота");
+
+        private String title;
+
+        DayOfWeek(String title) {
+            this.title = title;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public String toString() {
+            return "DayOfWeek{" +
+                    "title='" + title + '\'' +
+                    '}';
+        }
+    }
+
+    public class ScholarSchedule {
+
+        private DayOfWeek dayOfWeek;
+        //...другие поля
+
+
+        public DayOfWeek getDayOfWeek() {
+            return dayOfWeek;
+        }
+
+        public void setDayOfWeek(DayOfWeek dayOfWeek) {
+            this.dayOfWeek = dayOfWeek;
+        }
+    }
+
+    public class Scholar {
+
+        private ScholarSchedule schedule;
+        private boolean goToSchool;
+
+        public void wakeUp() {
+
+            if (this.schedule.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                System.out.println("Ура, можно поспать еще!");
+            } else {
+                System.out.println("Блин, опять в школу:(");
+            }
+        }
     }
 }
